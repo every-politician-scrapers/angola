bdfb7d6231d1a2e6a91944e09656011daf3f6d8b
Tony Bowden <tony@tmtm.com>
2021-08-26 11:32:12 +0300

    Also scrape date and place of birth

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -1,8 +1,31 @@
 #!/bin/env ruby
 # frozen_string_literal: true
 
+require 'date'
+
 require 'every_politician_scraper/scraper_data'
 require 'pry'
+
+class DatePT
+  MONTHS = %w[
+    null Janeiro Fevereiro Março Abril Maio Junho Julho Agosto Setembro Outubro Novembro Dezembro
+  ].freeze
+
+  def initialize(str)
+    @str = str
+  end
+
+  def to_s
+    str.sub(/(\d+) de (.*?) de (\d+).*/) do
+      day, mon, year = Regexp.last_match.captures
+      format('%d-%02d-%02d', year, MONTHS.index(mon), day)
+    end
+  end
+
+  private
+
+  attr_reader :str
+end
 
 class MemberList
   class Member
@@ -12,6 +35,18 @@ class MemberList
 
     def position
       noko.css('h4').text.tidy
+    end
+
+    field :dob do
+      DatePT.new(raw_dob).to_s
+    end
+
+    field :birth_place do
+      noko.xpath('.//span[contains(text(),"Naturalidade")]/following-sibling::text()[1]').text.tidy
+    end
+
+    def raw_dob
+      noko.xpath('.//div[span]/following-sibling::div[1]').text.tidy
     end
   end
 

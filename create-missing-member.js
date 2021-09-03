d991cedec2cf0f92ba4657ba51fda4f65bd22b96
Tony Bowden <tony@tmtm.com>
2021-09-03 09:13:09 +0300

    tweak create-missing-member to include DOBs

--- create-missing-member.js
+++ create-missing-member.js
@@ -2,18 +2,31 @@ const fs = require('fs');
 let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
-module.exports = (label) => {
+module.exports = (label, dob) => {
   return {
     type: 'item',
     labels: {
       en: label,
+      pt: label,
     },
     descriptions: {
-      en: `politician in ${meta.jurisdiction.name}`,
+      en: 'Angolan politician',
+      ot: 'político angolano',
     },
     claims: {
       P31: { value: 'Q5' }, // human
       P106: { value: 'Q82955' }, // politician
+      P569: {
+        value: dob,
+        references: {
+          P854: meta.source.url,
+          P1476: {
+            text: meta.source.title,
+            language: meta.source.lang.code,
+          },
+          P813: new Date().toISOString().split('T')[0],
+        }
+      }
     }
   }
 }

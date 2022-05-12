4b4db8f61600b44b9bbd26f94c59af8bddfea5d6
Tony Bowden <tony@tmtm.com>
2022-05-12 13:18:43 +0300

    add remaining Term 4 data

--- sources/assembly-4-ep/create-missing-member.js
+++ sources/assembly-4-ep/create-missing-member.js
@@ -2,7 +2,31 @@ const fs = require('fs');
 let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
-module.exports = (label,gender,party) => {
+// https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
+String.prototype.toTitleCase = function() {
+  var i, j, str, lowers, uppers;
+  str = this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+
+  // leave as lower-case
+  lowers = ['Da', 'De', 'Dos'];
+  for (i = 0, j = lowers.length; i < j; i++)
+    str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'), 
+      function(txt) {
+        return txt.toLowerCase();
+      });
+
+  // leave as upper-case
+  uppers = [];
+  for (i = 0, j = uppers.length; i < j; i++)
+    str = str.replace(new RegExp('\\b' + uppers[i] + '\\b', 'g'), 
+      uppers[i].toUpperCase());
+
+  return str;
+}
+
+module.exports = (label,party,gender) => {
   mem = {
     value: meta.position,
     qualifiers: {
@@ -26,7 +50,7 @@ module.exports = (label,gender,party) => {
 
   return {
     type: 'item',
-    labels: { en: label, pt: label },
+    labels: { en: label.toTitleCase(), pt: label.toTitleCase() },
     descriptions: { en: 'politician in Angola' },
     claims: claims,
   }

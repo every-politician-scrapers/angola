fe95fc0362c29d6167c7e241ed328d8496537598
Tony Bowden <tony@tmtm.com>
2022-06-17 16:34:59 +0300

    modernise scraper

--- sources/attorney-general/scraper.rb
+++ sources/attorney-general/scraper.rb
@@ -4,46 +4,21 @@
 require 'every_politician_scraper/scraper_data'
 require 'pry'
 
-class OfficeholderNonTable < OfficeholderListBase::OfficeholderBase
-  def empty?
-    too_early?
-  end
-
-  def combo_date?
-    true
-  end
-
-  def raw_combo_date
-    raise 'need to define a raw_combo_date'
-  end
-
-  def name_node
-    raise 'need to define a name_node'
-  end
-
-  def too_early?
-    start_year < ignore_before
-  end
-end
-
-
 class OfficeholderList < OfficeholderListBase
   decorator RemoveReferences
-  # decorator UnspanAllTables
   decorator WikidataIdsDecorator::Links
 
-  # TODO: make this easier to override
   def holder_entries
     noko.xpath("//h2[.//span[contains(.,'List of attorneys general')]]//following-sibling::ul[1]//li")
   end
 
-  class Officeholder < OfficeholderNonTable
-    def raw_combo_date
-      noko.text.scan(/\((.*?)\)/).flatten.last
+  class Officeholder < OfficeholderNonTableBase
+    def name_node
+      noko.css('a').last
     end
 
-    def name_node
-      noko.css('a').first
+    def raw_combo_date
+      noko.text.scan(/\((.*?)\)/).flatten.last
     end
   end
 end
